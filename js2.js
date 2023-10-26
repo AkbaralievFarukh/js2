0fbe03d03517c3497e3400770a6725a12912a87e
Farukh <akbfarukh@gmail.com>
2023-10-26 19:53:15 +0600

    modifed

--- js2.js
+++ js2.js
@@ -1,2 +1,13 @@
-let a = 'hello'
-let b = 'Ivan'
+let a
+let b
+
+a = 'hello';
+b = 'Ivan';
+a = a.charAt(0).toUpperCase() + a.slice(1).toLowerCase();
+b = b.charAt(0).toLowerCase() + b.slice(1).toLowerCase();
+console.log(a + ' ' + b);
+
+a = 'lorem ipsum dolor';
+b = a.length;
+console.log(b);
+
