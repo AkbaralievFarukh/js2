f7858bbb5d81a694104b1e82f547fedc94464aa4
Farukh <akbfarukh@gmail.com>
2023-10-28 00:28:17 +0600

    modified

--- js2.js
+++ js2.js
@@ -1,18 +1,27 @@
-let a
-let b
+// a = 'hello';
+// b = 'Ivan';
+// a = a.charAt(0).toUpperCase() + a.slice(1).toLowerCase();
+// b = b.charAt(0).toLowerCase() + b.slice(1).toLowerCase();
+// console.log(a + ' ' + b);
 
-a = 'hello';
-b = 'Ivan';
-a = a.charAt(0).toUpperCase() + a.slice(1).toLowerCase();
-b = b.charAt(0).toLowerCase() + b.slice(1).toLowerCase();
-console.log(a + ' ' + b);
+// a = 'lorem ipsum dolor';
+// b = a.length;
+// console.log(b);
 
-a = 'lorem ipsum dolor';
-b = a.length;
-console.log(b);
+// a = 'lorem ipsum dolor sit amet'
+// let words = a.split(' ')
+// let reversedWords = [words[3], ...words.slice(0, 3), words[4]]
+// let result = reversedWords.join(' ')
+// console.log(result)
 
-a = 'lorem ipsum dolor sit amet'
-let words = a.split(' ')
-let reversedWords = [words[3], ...words.slice(0, 3), words[4]]
-let result = reversedWords.join(' ')
-console.log(result)
+// a = 'children'
+// b = a.slice(0, -1).toUpperCase() + a.slice(-1).toLowerCase();
+// console.log(b)
+
+// a = 'boss'
+// b = a[0] + a[2] + a[3] + a[1]
+// console.log(b);
+
+// a = sunday
+// b = a.
+// console.log(b)
